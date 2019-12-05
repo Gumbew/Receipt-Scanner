d9077f9182c64a6fe91f7ea2a35ff8fbe921eb81
Oles Pankiv <oles.pankiv@gmail.com>
2019-12-05 14:36:32 +0200

    Implement parse_header

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 
 from ocr import ReceiptScannerOCR
+from parse_header import parse_header
 from pprint import pprint
+import sys
 
-image_path = r"images\3.jpg"
+#image_path = r"images/2.jpg"
+image_path = sys.argv[1]
+
 receipt_dict = ReceiptScannerOCR.image_to_text(image_path, True)
-pprint(receipt_dict)
-
+pprint(receipt_dict['header'])
