f4b28f3b46db8a4db461fc9dc5f2955d201fbac5
Oles Pankiv <oles.pankiv@gmail.com>
2019-12-05 11:04:35 +0200

    Add shebang to main.py

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
+#!/usr/bin/python
+
 from receipt_scanner_ocr import ReceiptScannerOCR
 from pprint import pprint
 
 image_path = r"images\3.jpg"
 receipt_dict = ReceiptScannerOCR.image_to_text(image_path, False)
 pprint(receipt_dict)
-
-
-
-
